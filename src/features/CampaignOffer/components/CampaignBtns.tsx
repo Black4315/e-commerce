21c77a7d9ab4a951bf813c712379e4fdaaae1f98
Black4315 <yurytube4315@gmail.com>
2025-07-24 11:55:59 +0300

    camaign Btns

--- src/features/CampaignOffer/components/CampaignBtns.tsx
+++ src/features/CampaignOffer/components/CampaignBtns.tsx
@@ -5,20 +5,24 @@ import { useCampaignOfferContxt } from "../contexts"
 import Link from "next/link"
 
 
-const CampaignBtns = ({ timerEnded }: { timerEnded :boolean}) => {
+const CampaignBtns = ({ timerEnded }: { timerEnded: boolean }) => {
     const t = useTranslations('homePage')
-    const {data} = useCampaignOfferContxt()
+    const { data } = useCampaignOfferContxt()
 
     return (
-        <div className="flex gap-6 items-center">
-            <Button 
-                className={`bg-button-1 hover:bg-button-1/80 semi-text text-text-1 px-6 md:px-12 py-2 md:py-4 w-auto h-auto ${timerEnded?"brightness-[0.6] grayscale-[0.4] pointer-events-none":""}`}>{
-            t('buyNowBtn')}!
+        <div className="flex gap-3 items-center">
+            <Button
+                className={`bg-button-1 hover:bg-button-1/80 semi-text text-text-1 px-6 md:px-12 py-2 md:py-4 w-auto h-auto ${timerEnded ? "brightness-[0.6] grayscale-[0.6] pointer-events-none" : ""}`}>{
+                    t('buyNowBtn')}!
             </Button>
 
-            {timerEnded && <Link href={data.campaign.fallback_after_expiry.redirect_url}>
-                {data.campaign.fallback_after_expiry.button}
-            </Link>
+            {timerEnded &&
+                <Button
+                    className={`bg-button-1 hover:bg-button-1/80 semi-text text-text-1 px-2 md:px-12 py-2 md:py-4 w-auto h-auto order-1 capitalize`}>
+                    <Link href={data.campaign.fallback_after_expiry.redirect_url}>
+                        {data.campaign.fallback_after_expiry.button}
+                    </Link>
+                </Button>
             }
         </div>
     )
