5d625866ff32529f1984623805a2b2f5081d096e
Black4315 <yurytube4315@gmail.com>
2025-10-07 18:41:54 +0300

    progink

--- src/entities/Product/components/ProductCard/components/ProductCardImage.tsx
+++ src/entities/Product/components/ProductCard/components/ProductCardImage.tsx
@@ -1,8 +1,8 @@
 import { useMobileCheck } from "@/contexts/MobileCheckContext";
-import Link from "next/link";
 import { useProductContext } from "../../../contexts/ProductContext";
 import CustomImage from "@/components/ui/CustomImag";
 import { useProductSelection } from "@/entities/Product/contexts/ProductSelectionContext";
+import ProgLink from "@/utils/ProgLink";
 
 const ProductCardImage = () => {
   const isMobile = useMobileCheck();
@@ -11,7 +11,7 @@ const ProductCardImage = () => {
 
   const thumbnail = selectedVariant?.images[0];
   return (
-    <Link
+    <ProgLink
       href={`/products/${handle}`}
       className={`absolute top-0 left-0 bottom-0 w-full flex flex-col h-full transition-apple duration-200 ${
         !isMobile ? "group-hover/img:brightness-90" : "brightness-[0.97]"
@@ -26,7 +26,7 @@ const ProductCardImage = () => {
           className="object-contain max-h-9/12 w-8/12 md:w-9/12"
         />
       </div>
-    </Link>
+    </ProgLink>
   );
 };
 
